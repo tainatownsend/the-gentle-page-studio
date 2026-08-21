74f6ff6e358406916fc6aced1ca024ac6c8d1010
Taina Townsend <40957192+tainatownsend@users.noreply.github.com>
2026-08-20 22:33:21 -0700

    feat(publications): export PDF planning contract

---
+++ src/studio/publications/export/index.ts
@@ -0,0 +1,13 @@
+export {
+  createPublicationPdfPlan,
+  PDF_POINTS_PER_INCH,
+  PUBLICATION_MARGIN_POINTS,
+  US_LETTER_HEIGHT_POINTS,
+  US_LETTER_WIDTH_POINTS,
+} from './publicationPdfPlan'
+export type {
+  PublicationPdfInteractiveField,
+  PublicationPdfInteractiveFieldKind,
+  PublicationPdfPagePlan,
+  PublicationPdfPlan,
+} from './publicationPdfPlan'
