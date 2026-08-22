502aa68a7421b1014216096a132c2cfcb2956c87
Taina Townsend <40957192+tainatownsend@users.noreply.github.com>
2026-08-21 20:50:06 -0700

    feat(publications): export revision comparison

--- src/studio/publications/utils/index.ts
+++ src/studio/publications/utils/index.ts
@@ -1,2 +1,7 @@
+export { comparePublicationRevisions } from './comparePublicationRevisions'
+export type {
+  PublicationRevisionChange,
+  PublicationRevisionComparison,
+} from './comparePublicationRevisions'
 export { createPublicationId } from './createPublicationId'
 export { formatPublicationUpdatedAt } from './formatPublicationUpdatedAt'
